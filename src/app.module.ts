8ffbe306d328326759965c279780f0fcc14023a9
Maksym Sydorenko <maksym.sy96@gmail.com>
2023-12-24 10:56:53 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonModule } from './lesson/lesson.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
